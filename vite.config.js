0e39bdaa1b804e2a0001e8e8660ea818466cfd05
Blairon Gonçalves De Sousa <euuvlash@gmail.com>
2025-06-27 18:11:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // Necessário para o alias
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Sistema-de-Cadastro-e-Avalia-o/', // 👈 Nome exato do seu repositório
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Alias '@' apontando para a sua pasta 'src' principal
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
